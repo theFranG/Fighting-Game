829200c2e7ee78e9180a5dfc83a467a2f917b00e
Franco <francogomez23800@gmail.com>
2023-08-18 23:08:39 -0300

    sdsa

--- index.js
+++ index.js
@@ -32,119 +32,124 @@ const shop = new Sprite({
 
 const player = new Fighter({
     position: {
-        x: 0,
-        y: 0
+      x: 0,
+      y: 0
     },
     velocity: {
-        x: 0,
-        y: 0
+      x: 0,
+      y: 0
     },
     offset: {
-        x: 0,
-        y: 0
+      x: 0,
+      y: 0
     },
-    imageSrc: './assets/samuraiMack/Idle.png',
-    frameMax: 8,
+    imageSrc: './img/samuraiMack/Idle.png',
+    framesMax: 8,
     scale: 2.5,
     offset: {
-        x: 0,
-        y: 157
+      x: 215,
+      y: 157
     },
     sprites: {
-        idle: {
-            imageSrc: './assets/samuraiMack/Idle.png',
-            frameMax: 8
-        },
-        run: {
-            imageSrc: './assets/samuraiMack/Run.png',
-            frameMax: 8,
-        },
-        jump: {
-            imageSrc: './assets/samuraiMack/Jump.png',
-            frameMax: 2
-        },
-        fall: {
-            imageSrc: './assets/samuraiMack/Fall.png',
-            frameMax: 2
-        },
-        attack1: {
-            imageSrc: './assets/SamuraiMack/Attack1.png',
-            frameMax: 6
-        }, takeHit: {
-            imageSrc: './assets/SamuraiMack/Take hit - white silhouette.png',
-            frameMax: 4
-        }, death: {
-            imageSrc: './assets/samuraiMack/Death.png',
-            frameMax: 6
-        }
+      idle: {
+        imageSrc: './img/samuraiMack/Idle.png',
+        framesMax: 8
+      },
+      run: {
+        imageSrc: './img/samuraiMack/Run.png',
+        framesMax: 8
+      },
+      jump: {
+        imageSrc: './img/samuraiMack/Jump.png',
+        framesMax: 2
+      },
+      fall: {
+        imageSrc: './img/samuraiMack/Fall.png',
+        framesMax: 2
+      },
+      attack1: {
+        imageSrc: './img/samuraiMack/Attack1.png',
+        framesMax: 6
+      },
+      takeHit: {
+        imageSrc: './img/samuraiMack/Take Hit - white silhouette.png',
+        framesMax: 4
+      },
+      death: {
+        imageSrc: './img/samuraiMack/Death.png',
+        framesMax: 6
+      }
+    },
+    attackBox: {
+      offset: {
+        x: 100,
+        y: 50
+      },
+      width: 160,
+      height: 50
     }
-    , attackBox: {
-        offset: {
-            x: 100,
-            y: 50
-        },
-        width: 160,
-        height: 50
-    }
-})
-
-const enemy = new Fighter({
+  })
+  
+  const enemy = new Fighter({
     position: {
-        x: 400,
-        y: 100
+      x: 400,
+      y: 100
     },
     velocity: {
-        x: 0,
-        y: 0
+      x: 0,
+      y: 0
     },
     color: 'blue',
     offset: {
-        x: -50,
-        y: 0
+      x: -50,
+      y: 0
     },
-    imageSrc: './assets/kenji/Idle.png',
-    frameMax: 4,
+    imageSrc: './img/kenji/Idle.png',
+    framesMax: 4,
     scale: 2.5,
     offset: {
-        x: 50,
-        y: 167
+      x: 215,
+      y: 167
     },
     sprites: {
-        idle: {
-            imageSrc: './assets/kenji/Idle.png',
-            frameMax: 4
-        },
-        run: {
-            imageSrc: './assets/kenji/Run.png',
-            frameMax: 8,
-        },
-        jump: {
-            imageSrc: './assets/kenji/Jump.png',
-            frameMax: 2
-        },
-        fall: {
-            imageSrc: './assets/kenji/Fall.png',
-            frameMax: 2
-        }, attack1: {
-            imageSrc: './assets/Kenji/Attack1.png',
-            frameMax: 4
-        }, takeHit: {
-            imageSrc: './assets/kenji/Take hit.png',
-            frameMax: 3
-        }, death: {
-            imageSrc: './assets/Kenji/Death.png',
-            frameMax: 7
-        }
+      idle: {
+        imageSrc: './img/kenji/Idle.png',
+        framesMax: 4
+      },
+      run: {
+        imageSrc: './img/kenji/Run.png',
+        framesMax: 8
+      },
+      jump: {
+        imageSrc: './img/kenji/Jump.png',
+        framesMax: 2
+      },
+      fall: {
+        imageSrc: './img/kenji/Fall.png',
+        framesMax: 2
+      },
+      attack1: {
+        imageSrc: './img/kenji/Attack1.png',
+        framesMax: 4
+      },
+      takeHit: {
+        imageSrc: './img/kenji/Take hit.png',
+        framesMax: 3
+      },
+      death: {
+        imageSrc: './img/kenji/Death.png',
+        framesMax: 7
+      }
     },
     attackBox: {
-        offset: {
-            x: -170,
-            y: 50
-        },
-        width: 170,
-        height: 50
+      offset: {
+        x: -170,
+        y: 50
+      },
+      width: 170,
+      height: 50
     }
-})
+  })
 
 const keys = {
     a: {
@@ -173,8 +178,8 @@ function animate() {
     player.update()
     enemy.update()
 
-    player.velocity.x = 2
-    enemy.velocity.x = 2
+    player.velocity.x = 0
+    enemy.velocity.x = 0
 
     //Player movement
 
